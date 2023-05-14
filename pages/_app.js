9ace1acfd45bac662a0dafccb0f24a00c66f74b8
Mure01 <belci911mu@gmail.com>
2023-05-14 19:13:08 +0200

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,11 @@ import '@/styles/globals.css'
 import Layout from '@/components/Layout'
 import StateContext from '@/context/StateContext'
 import Head from "next/head";
-import fav from '../public/logo.jpg'
 export default function App({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
         <Head>
-        <link rel="icon" type="image/jpg" href={fav}/>
         <title>Ideal namještaj</title>
         </Head>
         <Component {...pageProps} />
